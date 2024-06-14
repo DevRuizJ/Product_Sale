2c4bd3358bddfc59017327a4d0cb7c5cb5aaf3f6
DevJARM <repo.ruiz.javier@gmail.com>
2024-06-14 10:43:54 -0500

    se añade diseño al dashboard

--- ProductSale_UI/src/layouts/Dashboard/dashboard.vue
+++ ProductSale_UI/src/layouts/Dashboard/dashboard.vue
@@ -1,27 +1,26 @@
 <template>
-  <!-- <q-page class="dashboard-container">
-    <q-card class="q-pa-md" style="max-width: 800px; margin: auto;">
-      <q-card-section>
-        <q-input outlined v-model="searchQuery" placeholder="Search products..." />
-        <q-select outlined v-model="selectedCategory" label="Category" :options="categoryOptions" />
-      </q-card-section>
+  <q-layout>
+    <q-page-container>
+      <q-card class="q-pa-md" style="max-width: 800px; margin: auto;">
+        <q-card-section>
+          <q-input outlined v-model="searchQuery" placeholder="Search products..." />
+          <q-select outlined v-model="selectedCategory" label="Category" :options="categoryOptions" />
+        </q-card-section>
 
-      <q-separator />
+        <q-separator />
 
-      <q-card-section class="q-pa-md">
-        <q-card v-for="product in filteredProducts" :key="product.id" class="product-card">
-          <q-img :src="product.image" :alt="product.name" class="q-mb-sm" />
-          <q-card-section>
-            <q-card-title>{{ product.name }}</q-card-title>
-            <q-card-text class="text-grey">{{ /*product.price | currency*/ '1000.00' }}</q-card-text>
-          </q-card-section>
-        </q-card>
-      </q-card-section>
-    </q-card>
-  </q-page> -->
-  <div>
-    <p>aquí iría el dashboard</p>
-  </div>
+        <q-card-section class="q-pa-md">
+          <q-card v-for="product in filteredProducts" :key="product.id" class="product-card">
+            <q-img :src="product.image" :alt="product.name" class="q-mb-sm" />
+            <q-card-section>
+              <q-card-title>{{ product.name }}</q-card-title>
+              <q-card-text class="text-grey">{{ /*product.price | currency*/'1000.00' }}</q-card-text>
+            </q-card-section>
+          </q-card>
+        </q-card-section>
+      </q-card>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script lang="ts">
@@ -29,9 +28,10 @@ import { defineComponent, ref, computed } from 'vue'
 
 export default defineComponent({
   name: 'dash_board',
-  setup () {
-    const searchQuery = ref<string>('')
-    const selectedCategory = ref<string>('')
+  // eslint-disable-next-line space-before-function-paren
+  setup() {
+    const searchQuery = ref('')
+    const selectedCategory = ref('')
 
     const products = ref([
       { id: 1, name: 'Product 1', price: 100, image: '/path-to-image1.jpg', category: 'electronics' },
